db556bb69805c719474c776152ad29570dbae6b9
samsiljee <samsiljee@gmail.com>
2024-01-31 17:39:31 +1300

    Now reading in card data again

--- practiceScript.R
+++ practiceScript.R
@@ -14,3 +14,9 @@ cards
 data <- fromJSON(rawToChar(cards$content))
 
 card_data <- data$data
+
+# Write card data to cache for offline use
+write.csv(card_data, "prettyMessenger/cache/card_data.csv")
+
+# Read in the card data again from the cache
+card_data <- read.csv("prettyMessenger/cache/card_data.csv")
